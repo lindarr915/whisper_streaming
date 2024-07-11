e06eeed42ada8f69bb8987a83dac29e80842ae72
Darren Lin <lindarr@amazon.com>
2024-07-11 03:52:42 +0000

    add build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,15 @@
 # CI
 
 aws ecr get-login-password --region ap-northeast-1 --profile ray-cluster | docker login --username AWS --password-stdin 464616699298.dkr.ecr.ap-northeast-1.amazonaws.com
-docker build -t streaming-asr .
+docker build -t streaming-asr . 
 docker tag streaming-asr 464616699298.dkr.ecr.ap-northeast-1.amazonaws.com/streaming-asr
 docker push 464616699298.dkr.ecr.ap-northeast-1.amazonaws.com/streaming-asr  
+
+
+docker tag streaming-asr public.ecr.aws/darrenlin/whisper-streaming-v2:latest
+docker push public.ecr.aws/darrenlin/whisper-streaming-v2:latest
 
 # CD 
 kubectl delete 
 kubeclt apply 
+
